63b9e1cbe32b9fa2dec158e320a76619a1ced72f
Benjamin Lapointe-Pinel <benjamin.lapointe13@gmail.com>
2022-04-02 10:29:40 -0400

    created project

---
+++ 8INF957-MS_TP2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _8INF957_MS_TP2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
