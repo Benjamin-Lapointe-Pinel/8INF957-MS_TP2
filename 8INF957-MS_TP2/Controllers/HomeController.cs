d11878603d544b31803bb0e4144804eeed5f3eb1
zaidtidjet <tidz0001@uqar.ca>
2022-04-02 09:06:43 -0700

    Connexion

--- 8INF957-MS_TP2/Controllers/HomeController.cs
+++ 8INF957-MS_TP2/Controllers/HomeController.cs
@@ -6,14 +6,8 @@ namespace _8INF957_MS_TP2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        public IActionResult Connexion()
         {
             return View();
         }
@@ -23,10 +17,6 @@ namespace _8INF957_MS_TP2.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
