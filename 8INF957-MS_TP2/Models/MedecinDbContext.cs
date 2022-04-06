00e41f4a7f4af770a8dee7ee1b957b45cbcc5fae
zaidtidjet1 <tidz0001@uqar.ca>
2022-04-06 14:07:33 -0700

    Class Model Medecin et MedecinDbContext

---
+++ 8INF957-MS_TP2/Models/MedecinDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _8INF957_MS_TP2.Models
+{
+
+    public class MedecinDbContext : DbContext
+    {
+        public MedecinDbContext( DbContextOptions <MedecinDbContext> options)
+            :base(options)
+        {
+
+           
+        }
+        public DbSet <Patient> Patients { get; set; }
+        public DbSet <Medecin> Medecins { get; set; }
+    }
+}
